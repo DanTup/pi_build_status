116149f36d3256b02f2f91614c7acc1424a8aa86
Danny Tuppeny <danny@tuppeny.com>
2019-08-29 09:42:19 +0100

    Use IP :(

--- bin/update.dart
+++ bin/update.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
 import 'dart:math';
 
-import 'package:pi_build_status/src/build_results.dart';
-import 'package:pi_build_status/src/flutter_build.dart' as flutter;
-import 'package:pi_build_status/src/dart_code_build.dart' as dartCode;
 import 'package:collection/collection.dart';
 import 'package:json_rpc_2/json_rpc_2.dart' as json_rpc;
+import 'package:pi_build_status/src/build_results.dart';
+import 'package:pi_build_status/src/dart_code_build.dart' as dartCode;
+import 'package:pi_build_status/src/flutter_build.dart' as flutter;
 import 'package:pi_build_status/src/utils.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/status.dart' as status;
@@ -155,7 +155,7 @@ List<int> charToColour(String status) {
 }
 
 Future sendPixels(List<List<List<int>>> pixels) async {
-  var socket = IOWebSocketChannel.connect('ws://officepi.local:8050');
+  var socket = IOWebSocketChannel.connect('ws://192.168.0.115:8050');
   var client = new json_rpc.Client(socket.cast<String>());
   client.listen();
 
